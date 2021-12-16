79210d586689cb331d4d52804e5d5b537d49e3ee
BennoAlif <bennoalif41@gmail.com>
2021-12-16 16:12:09 +0700

    [Splash Page] Footer

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -8,62 +8,73 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 50,
-            left: 30,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 50,
-                width: 50,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/logo.png'),
-                  ),
-                ),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                'assets/splash_image.png',
               ),
-              const SizedBox(
-                height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 24,
+                left: 30,
               ),
-              Text(
-                "Find Cozy House\nto Stay and Happy",
-                style: blackTextStyle.copyWith(
-                  fontSize: 24,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Stop membuang banyak waktu\npada tempat yang tidak habitable",
-                style: greyTextStyle.copyWith(
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              SizedBox(
-                width: 210,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(17),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/logo.png'),
+                      ),
                     ),
                   ),
-                  child: Text(
-                    "Explore Now",
-                    style: whiteTextStyle.copyWith(fontSize: 18),
+                  const SizedBox(
+                    height: 30,
                   ),
-                ),
-              )
-            ],
-          ),
+                  Text(
+                    "Find Cozy House\nto Stay and Happy",
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Stop membuang banyak waktu\npada tempat yang tidak habitable",
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  SizedBox(
+                    width: 210,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: purpleColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                      ),
+                      child: Text(
+                        "Explore Now",
+                        style: whiteTextStyle.copyWith(fontSize: 18),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
