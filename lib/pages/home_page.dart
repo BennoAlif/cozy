994f5ff54e35ef5e064d96b19430dd6758443803
BennoAlif <bennoalif41@gmail.com>
2021-12-25 09:48:39 +0700

    [Home Page] Recommended Spaces

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:bwa_cozy/models/city.dart';
+import 'package:bwa_cozy/models/space.dart';
 import 'package:bwa_cozy/theme.dart';
 import 'package:bwa_cozy/widgets/city_card.dart';
+import 'package:bwa_cozy/widgets/space_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,122 +12,176 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            vertical: edge,
-          ),
-          child: ListView(
-            children: [
-              Container(
-                margin: EdgeInsets.only(left: edge),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Explore Now",
-                      style: blackTextStyle.copyWith(
-                        fontSize: 24,
-                      ),
-                    ),
-                    Text(
-                      "Mencari kosan yang cozy",
-                      style: greyTextStyle.copyWith(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: edge),
-                child: Text(
-                  "Popular Cities",
-                  style: regularTextStyle.copyWith(
-                    fontSize: 16,
+        // bottom: false,
+        child: ListView(
+          children: [
+            // * Title/Header
+            Container(
+              margin: EdgeInsets.only(left: edge),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: edge,
                   ),
+                  Text(
+                    "Explore Now",
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                    ),
+                  ),
+                  Text(
+                    "Mencari kosan yang cozy",
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                "Popular Cities",
+                style: regularTextStyle.copyWith(
+                  fontSize: 16,
                 ),
               ),
-              const SizedBox(
-                height: 16,
-              ),
-              SizedBox(
-                height: 150,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    const SizedBox(
-                      width: 24,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  const SizedBox(
+                    width: 24,
+                  ),
+                  CityCard(
+                    city: City(
+                      id: 1,
+                      name: 'Jakarta',
+                      imageUrl: 'assets/city1.png',
                     ),
-                    CityCard(
-                      city: City(
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  CityCard(
+                    city: City(
+                      id: 2,
+                      name: 'Bandung',
+                      imageUrl: 'assets/city2.png',
+                      isPopular: true,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  CityCard(
+                    city: City(
+                      id: 3,
+                      name: 'Surabaya',
+                      imageUrl: 'assets/city3.png',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  CityCard(
+                    city: City(
+                      id: 4,
+                      name: 'Palembang',
+                      imageUrl: 'assets/city4.png',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  CityCard(
+                    city: City(
+                      id: 5,
+                      name: 'Aceh',
+                      imageUrl: 'assets/city5.png',
+                      isPopular: true,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  CityCard(
+                    city: City(
+                      id: 6,
+                      name: 'Bogor',
+                      imageUrl: 'assets/city6.png',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 24,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                "Recommended Space",
+                style: regularTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: edge,
+              ),
+              child: Column(
+                children: [
+                  SpaceCard(
+                    space: Space(
                         id: 1,
-                        name: 'Jakarta',
-                        imageUrl: 'assets/city1.png',
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    CityCard(
-                      city: City(
-                        id: 2,
-                        name: 'Bandung',
-                        imageUrl: 'assets/city2.png',
-                        isPopular: true,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    CityCard(
-                      city: City(
-                        id: 3,
-                        name: 'Surabaya',
-                        imageUrl: 'assets/city3.png',
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    CityCard(
-                      city: City(
-                        id: 4,
-                        name: 'Palembang',
-                        imageUrl: 'assets/city4.png',
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    CityCard(
-                      city: City(
-                        id: 5,
-                        name: 'Aceh',
-                        imageUrl: 'assets/city5.png',
-                        isPopular: true,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    CityCard(
-                      city: City(
-                        id: 6,
-                        name: 'Bogor',
-                        imageUrl: 'assets/city6.png',
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 24,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                        name: "Kuretakeso Hott",
+                        price: 52,
+                        city: "Bandung",
+                        country: "Indonesia",
+                        imageUrl: "assets/space1.png",
+                        rating: 4),
+                  ),
+                  SpaceCard(
+                    space: Space(
+                        id: 1,
+                        name: "Kuretakeso Hott",
+                        price: 52,
+                        city: "Bandung",
+                        country: "Indonesia",
+                        imageUrl: "assets/space2.png",
+                        rating: 4),
+                  ),
+                  SpaceCard(
+                    space: Space(
+                        id: 1,
+                        name: "Kuretakeso Hott",
+                        price: 52,
+                        city: "Bandung",
+                        country: "Indonesia",
+                        imageUrl: "assets/space3.png",
+                        rating: 4),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
---
+++ lib/widgets/space_card.dart
@@ -0,0 +1,104 @@
+import 'package:bwa_cozy/models/space.dart';
+import 'package:flutter/material.dart';
+
+import '../theme.dart';
+
+class SpaceCard extends StatelessWidget {
+  final Space space;
+  const SpaceCard({Key? key, required this.space}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        bottom: 30,
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: SizedBox(
+              width: 130,
+              height: 110,
+              child: Stack(
+                children: [
+                  Image.asset(
+                    space.imageUrl,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 70,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: purpleColor,
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(36),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icon_star.png',
+                            width: 22,
+                            height: 22,
+                          ),
+                          Text(
+                            "${space.rating}/5",
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 13,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                space.name,
+                style: blackTextStyle.copyWith(
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text.rich(
+                TextSpan(
+                  text: "\$${space.price}",
+                  style: purpleTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  children: [
+                    TextSpan(
+                        text: "/month",
+                        style: greyTextStyle.copyWith(
+                          fontSize: 16,
+                        )),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                '${space.city}, ${space.country}',
+                style: greyTextStyle,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
